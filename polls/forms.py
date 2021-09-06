0cf5563c69fad9aae949319ded417068cf2c1718
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-09-06 16:01:47 -0300

    Add URL shortener form

---
+++ polls/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from polls.models import Shortener
+
+class ShortenerForm(forms.ModelForm):
+    long_url = forms.URLField(widget=forms.URLInput(
+        attrs={"class": "form-control form-control-lg", "placeholder": "Your URL to shorten"}))
+    
+    class Meta:
+        model = Shortener
+        fields = ('long_url',)
+    
